70c76365f3049a591685d825d096b751fe2b3b45
Sharfraz Khan <151615710+blueblood897@users.noreply.github.com>
2025-03-17 17:20:55 +0600

    Add files via upload

---
+++ Loops/main.dart
@@ -0,0 +1,8 @@
+void main() {
+  String message = "Hello World";
+  for (int i = 0; i < 10; i++) {
+    message = message.substring(0, 5);
+    print("$message $i");
+  }
+  //similarly while loop and for while loop can be used..syntax is same as java or c++
+}
